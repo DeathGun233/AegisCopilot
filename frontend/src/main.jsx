ba7dd22ff7fd95c4ac135157e4c13a1d84368496
DeathGun <1820056013@qq.com>
2026-03-30 20:45:36 +0800

    优化知识库后台筛选体验

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { AppAdminConsole } from "./AppAdminConsole";
+import { AppProductConsole } from "./AppProductConsole";
 import "./styles-next.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <AppAdminConsole />
+    <AppProductConsole />
   </React.StrictMode>,
 );
